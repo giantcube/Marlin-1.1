66e7468b27876f790724fe504a459eb767645bb8
Hamid FzM <hamidfzm@gmail.com>
2018-05-13 23:32:54 +0430

    Calibrate xyz axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -529,7 +529,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 775.32, 210 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 158.25, 160.40, 801.36, 210 }
 
 /**
  * Default Max Feed Rate (mm/s)
