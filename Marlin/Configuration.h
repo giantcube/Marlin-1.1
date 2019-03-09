9cc37cd04c7eb3f141693633c090bacd9906627d
Hamid FzM <hamidfzm@gmail.com>
2019-03-09 23:51:44 +0330

    Add latest config files

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -529,7 +529,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 158.25, 160.40, 801.36, 210 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 158.25, 160.40, 801.36, 200 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -780,8 +780,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 160
+#define Y_BED_SIZE 120
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
